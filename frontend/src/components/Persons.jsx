f4e3d92b9fc9a497d117bc3919fc5de8b75bcf1b
Lokesh <dontulalokesh@gmail.com>
2025-08-28 12:03:21 +0530

    updated repo for deployment

--- frontend/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ({ person, onDelete }) => {
-  return (
-    <p key={person.id}>
-      {person.name} {person.number}
-      <button onClick={() => onDelete(person.id)}>Delete</button>
-    </p>
-  );
-};
-export default Persons;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 let data = [
   {
     id: "1",
